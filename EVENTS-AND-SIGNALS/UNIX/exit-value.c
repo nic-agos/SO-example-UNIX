a11071d68486421d4dfd37007e2c94b151dcdb4d
Nicco <niccolo.agostinelli@gmail.com>
2021-11-12 15:45:25 +0100

    worked on signals and events in unix

--- EVENTS-AND-SIGNALS/UNIX/exit-value.c
+++ EVENTS-AND-SIGNALS/UNIX/exit-value.c
@@ -11,14 +11,14 @@ int main(int a, char **b){
 	int value;
 	
 	for (i=0; i<10; i++){
-		if(fork() == 0) pause();
+		if(fork() == 0) pause(); //se sono un child entro in pausa
 	}
 
-	signal(SIGINT,SIG_IGN);
+	signal(SIGINT,SIG_IGN);  // il parent va ad ignorare SIGINT
 
 	for (i=0; i<10; i++){
-		wait(&value);
-		printf("last two bytes of value are %d - %d\n",(value>>8)&255,value&255); 
+		wait(&value); // aspetto i processi figlio
+		printf("last two bytes of value are %d - %d\n",(value>>8)&255,value&255); // digitando ctrl+c da shell stampa 0 - 2
 	}
 	
 
--- EVENTS-AND-SIGNALS/UNIX/signal-numbers.c
+++ EVENTS-AND-SIGNALS/UNIX/signal-numbers.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 
 
-void generic_handler(int signal){
+void generic_handler(int signal){  //passiamo anche il codice numerico dell'evento verificatosi
 
 	printf("received signal is %d\n",signal);
 }
@@ -14,12 +14,13 @@ int main(int argc, char **argv){
 
   int  i;
 
+  //utilizzo lo stesso gestore per 3 diversi segnali
   signal(SIGINT,generic_handler);
   signal(SIGUSR1,generic_handler);
   signal(SIGUSR2,generic_handler);
 
   while(1) {
-	pause();
+	  pause();
   }
 
 }
