fac4aeb59029857774dc502a7f260ad1713b57a1
Niccolò Agostinelli <72873024+nic-agos@users.noreply.github.com>
2021-11-14 19:22:26 +0100

    Delete C-BASICS/UNIX directory

--- C-BASICS/UNIX/pointer-aritmetic.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int x;
-double *y;
-
-void main(void){
-	y = &x;
- 	printf("x adress is %p - y is %p | x address+1 is %p - y+1 is %p\n",&x,y,&x+1,y+1); /* y è già una variabile pointer quindi non serve & */
-}
