110484cdeaa80544cdc53720571bd638cc319073
Nicco <niccolo.agostinelli@gmail.com>
2021-11-16 17:27:30 +0100

    worked on sockets example

--- SOCKETS/UNIX/sockets-signals-options/echo-sigpipe-recvtimeout.c
+++ SOCKETS/UNIX/sockets-signals-options/echo-sigpipe-recvtimeout.c
@@ -1,3 +1,5 @@
+/*si comporta come server*/
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -19,7 +21,8 @@ int comm_sock;
 void generic_handler(int signal, siginfo_t *info, void* unused){
 
         printf("received signal is %d\n",signal);
-	close(comm_sock);
+        /*chiudo il socket di comunicazione, non mi serve più dato che il canale è rotto*/
+	close(comm_sock); 
 }
 
 
@@ -37,19 +40,22 @@ int main() {
 	int i;
 	int on = 1;
 
-	ds_sock = socket(AF_INET, SOCK_STREAM, 0);
+	/*creo un socket con dominio internet, modalità streaming, protocollo di default*/
+	ds_sock = socket(AF_INET, SOCK_STREAM, 0);  
 	if (ds_sock == -1){
 		printf("socket error\n");
 		return -1;
 	}
 	printf("socket installed - descriptor is %d\n",ds_sock);
 
-	setsockopt(ds_sock,SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+	/*attivo l'opzione di riusabilità dell'indirizzo di porta 25000*/
+	setsockopt(ds_sock,SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)); 
 
 	my_addr.sin_family      = AF_INET;
 	my_addr.sin_port        = port;
 	my_addr.sin_addr.s_addr = INADDR_ANY; 
 
+	/*associo al nostro socket l'indirizzo contenuto in my_addr*/
 	ret = bind(ds_sock,(struct sockaddr*) &my_addr, sizeof(my_addr));
 	if(ret == -1){
 		printf("bind error\n");
@@ -57,7 +63,9 @@ int main() {
 	}
 	printf("socket address set up ok\n");
 
+	/*porto il socket in stato listening */ 
 	ret = listen(ds_sock, BACKLOG);
+	
 	if(ret == -1){
 		printf("listen error\n");
 		return -1;
@@ -69,12 +77,19 @@ int main() {
 	act.sa_mask =  set;
   	act.sa_flags = SA_SIGINFO;
   	act.sa_restorer = NULL;
-  	sigaction(SIGPIPE,&act,NULL);
+
+  	/*imposto il gestore di segnale SIGPIPE che in sistemi Posix viene generato quando si 
+  	 rompe il canale, questo per evitare di andare a leggere/scrivere su canali rotti*/
+  	sigaction(SIGPIPE,&act,NULL); 
 
 
 
 	new_accept:
-	comm_sock =  accept(ds_sock,(struct sockaddr*)&their_addr,&size);
+
+	/*accetto una connessione sul listening socket e ottengo l'identificatore del nuovo socket generato 
+	 nella variabile globale comm_sock, cioè quella che viene chiusa in caso di ricezione di SIGPIPE */
+	comm_sock =  accept(ds_sock,(struct sockaddr*)&their_addr,&size); 
+	
 	if(comm_sock == -1){
 		printf("accept error\n");
 		return -1;
@@ -88,6 +103,7 @@ int main() {
 #ifdef RECEIVE_TIMEOUT
 	timeout.tv_sec = 5;
 	timeout.tv_usec = 0;
+	/*imposto il receive timeout a 5 secondi e 0 microsecondi solo sul socket di comunicazione (SOL_SOCKET) */
 	setsockopt(comm_sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
 #endif
 
@@ -102,34 +118,51 @@ int main() {
 
 	while(1){
 #ifdef ALARM
+		/*il timeout viene impostato manualmente con gli allarmi, lavorando a livello processo. 
+		 Con il receiver timeout invece posso agire sui singoli thread*/
 		alarm(5);
 #endif
 #ifndef PADDING
-		ret = read(comm_sock,buff,SIZE);
+		/*leggo dal socket usando read() dato che sto lavorando in modalità stream, quando compilo con 
+		 RECEIVE_TIMEOUT se entro 5 secondi non arrivano i dati allora il timeout mi sblocca dalla ricezione*/ 
+		ret = read(comm_sock,buff,SIZE); 
 #else
-		ret = recv(comm_sock,buff,SIZE,MSG_WAITALL);
+		/*MSG_WAITALL serve per indicare che il servizio mi deve mantenere in stato di blocco finchè questo non può estrarre 
+		 esattamente SIZE_MSG bytes dalla socket,  lo sblocco può avvenire alla ricezione di un segnale o quando arrivano tutti i dati*/
+		ret = recv(comm_sock,buff,SIZE,MSG_WAITALL); 
 		perror("");
 #endif
 #ifdef ALARM
 		alarm(0);
 #endif
 
-		if(ret<=0) break;
+		if(ret <= 0){
+			break;
+		}
+
 		printf("echoing this: ");
 		fflush(stdout);
-		write(1,buff,ret);
+		write(1,buff,ret); /*scrivo in stdout il contenuto del buffer, per una taglia di dati pari a ret byte*/
+
+		/*rispondo al client facendo echo di ciò che ho letto ECHO_LENGHT volte*/
 		for (i=0; i< ECHO_LENGTH; i++){
 			sleep(1);
 #ifndef HANDLE_SIGPIPE
+			/*scrivo sulla socket client ret bytes, non curandomi dei residui*/
 			ret = write(comm_sock,buff,ret);
 #else
+			/* MSG_NOSIGNAL: se il canale è rotto il segnale SIGPIPE lo voglio ignorare, accorgendomi dell'errore guardando il codice di ritorno della send() */
 			ret = send(comm_sock,buff,ret,MSG_NOSIGNAL);
 #endif
 		}
 	}
 	close(comm_sock);
+
 	printf("connection closed - accepting a new one\n");
+
 	fflush(stdout);
+	/*mi riporto in stato di accept di una nuova connessione riniziando il ciclo*/
 	goto new_accept;
+
 	return 0;
 }
--- SOCKETS/UNIX/sockets-signals-options/stream-sender.c
+++ SOCKETS/UNIX/sockets-signals-options/stream-sender.c
@@ -1,3 +1,5 @@
+/*si comporta come client*/
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -13,7 +15,7 @@ int ds_sock;
 void * get_echo(){
 	char buff[128];
 	while(1){
-		 write(1,buff,read(ds_sock,buff, 128));
+		write(1,buff,read(ds_sock,buff, 128));
 	}
 }
 
@@ -27,7 +29,7 @@ int main() {
 	int comm_sock;
 	pthread_t thread;
 
-	ds_sock = socket(AF_INET, SOCK_STREAM, 0);
+	ds_sock = socket(AF_INET, SOCK_STREAM, 0); /*creo il socket client*/
 	if (ds_sock == -1){
 		printf("socket error\n");
 		return -1;
@@ -38,7 +40,10 @@ int main() {
 	my_addr.sin_family      = AF_INET;
 	my_addr.sin_port        = htons(0);
 	inet_aton("127.0.0.1",&my_addr.sin_addr);
+
+	/*associo al mio socket l'indirizzo contenuto in my_addr*/
 	ret = bind(ds_sock, (struct sockaddr*)&my_addr, sizeof(my_addr));
+	
 	if(ret == -1){
 		printf("bind error\n");
 		return -1;
@@ -47,7 +52,10 @@ int main() {
 	memset(&their_addr, 0, sizeof(their_addr));
 	their_addr.sin_family      = AF_INET;
 	their_addr.sin_port        = port;
-	inet_aton("127.0.0.1",&their_addr.sin_addr);
+
+	/*l'indirizzo dell'host server è 127.0.0.1, 
+	 inet_aton trasforma l'indirizzo IP (formato stringa) nella sua rappresentazione in byte*/
+	inet_aton("127.0.0.1",&their_addr.sin_addr); 
 
 	ret = connect(ds_sock,(struct sockaddr*) &their_addr, sizeof(their_addr));
 	if(ret == -1){
@@ -56,9 +64,11 @@ int main() {
 	}
 	printf("socket connection set up ok\n");
 
-	pthread_create(&thread,NULL,get_echo,NULL);
+	/*creo un thread che leggera l'echo del server*/
+	pthread_create(&thread,NULL, get_echo,NULL);
 
-	while(1){//residuals are not handled
+	/*leggo dal server e scrivo in buff, i residui non vengono gestiti*/
+	while(1){	
 	  ret = read(0,buff, 128);
 	  write(ds_sock,buff,ret);
 	}
